acbbd7f6c68a0d78afd74a676a97d8cc2eed991f
dandeboer <ddeboer926@gmail.com>
2020-03-04 14:49:22 -0500

    habits now show up on habit list

--- core/views.py
+++ core/views.py
@@ -4,4 +4,5 @@ from django.shortcuts import render
 from .models import Habit, Result
 
 def habit_list(request):
-    return render(request, 'core/habit_list.html')
+    habits = Habit.objects.all()
+    return render(request, 'core/habit_list.html', {'habits': habits})
