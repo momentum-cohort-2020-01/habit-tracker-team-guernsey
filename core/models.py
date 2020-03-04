c3291869fc3548c331de657cd163ee894cc59f35
Geoff <gabishop35@gmail.com>
2020-03-04 13:09:49 -0500

    create models/admin setup

--- core/models.py
+++ core/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Habit(models.Model):
+    name = models.CharField(max_length=50)
+    goal = models.TextField()
+    date = models.DateField()
+    # we may need to change DateField
+    result = models.ForeignKey("Result", on_delete=models.SET_NULL, null=True, blank=True)
+
+class Result(models.Model):
+    recording = models.TextField()
+    goal_met = models.BooleanField(default=False)
+
+# class Observer:
