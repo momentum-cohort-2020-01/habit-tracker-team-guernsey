41dc01b7b58f3de4f86f012e0de8181b9b3bf5c4
Geoff <gabishop35@gmail.com>
2020-03-05 12:29:04 -0500

    add str to models

--- core/models.py
+++ core/models.py
@@ -10,8 +10,14 @@ class Habit(models.Model):
     # we may need to change DateField
     result = models.ForeignKey("Result", on_delete=models.SET_NULL, null=True, blank=True)
 
+    def __str__(self):
+        return f" Habit name: {self.name} goal: {self.goal} date: {self.date}"
+
 class Result(models.Model):
     recording = models.TextField()
     goal_met = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.name}"
 
 # class Observer:
